410523b2152e2475eba9e6a718eab956bedb65fe
Ye Jun Seo <147582310+ollajunseo@users.noreply.github.com>
2023-11-30 11:02:27 +0900

    script
    공용 스크립트입니다.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const scrollTopButton = document.getElementById('scrollTopButton');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
+
+scrollTopButton.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
